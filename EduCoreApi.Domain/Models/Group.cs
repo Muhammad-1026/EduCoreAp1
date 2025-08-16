9522ed096a5c591ec47d82845f9bbf6780c34e88
abubakrmirgiyasov <abubakrmirgiyasov@gmail.com>
2025-08-16 17:03:14 +0500

    feat(domain): add Subject entity and update Group model structure

--- EduCoreApi.Domain/Models/Group.cs
+++ EduCoreApi.Domain/Models/Group.cs
@@ -1,5 +1,62 @@
-﻿namespace EduCoreApi.Domain.Models;
+﻿using EduCoreApi.Shared.Exeptions;
+using EduCoreApi.Shared.Models;
+
+namespace EduCoreApi.Domain.Models;
 
 public class Group
 {
+    public Guid Id { get; private set; }
+
+    public string Name { get; private set; } = default!;
+
+    public int CourseNumber { get; private set; }
+
+    public string? Description { get; private set; }
+
+    public Group(string name, int courseNumber)
+    {
+        Name = name;
+        CourseNumber = courseNumber;
+    }
+
+    public void SetName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new BussinessLogicException(GroupErrors.NameCantBeNull);
+
+        Name = name;
+    }
+
+    public void SetCourseNumber(int courseNumber)
+    {
+        if (courseNumber <= 0)
+            throw new BussinessLogicException(GroupErrors.CourseNumberCantBeNull);
+
+        CourseNumber = courseNumber;
+    }
+
+    public void SetDescription(string? description)
+    {
+        if (string.IsNullOrWhiteSpace(description))
+            throw new BussinessLogicException(GroupErrors.DescriptionCantBeNull);
+        Description = description;
+    }
+
+    public class GroupErrors
+    {
+        public static readonly Error NameCantBeNull = new(
+            "Group.NameCantBeNull",
+            "Name cannot be null or empty."
+        );
+
+        public static readonly Error CourseNumberCantBeNull = new(
+            "Group.CourseNumberCantBeNull",
+            "Course number cannot be null or empty."
+        );
+
+        public static readonly Error DescriptionCantBeNull = new(
+            "Group.DescriptionCantBeNull",
+            "Description cannot be null or empty."
+        );
+    }
 }
---
+++ EduCoreApi.Domain/Models/Subject.cs
@@ -0,0 +1,46 @@
+﻿using EduCoreApi.Domain.Common;
+using EduCoreApi.Shared.Exeptions;
+using EduCoreApi.Shared.Models;
+
+namespace EduCoreApi.Domain.Models;
+
+public class Subject : Entity
+{
+    public string Name { get; private set; } = default!;
+
+    public string? Description { get; private set; }
+
+    public Subject(string name)
+    {
+        Name = name;
+    }
+
+    public void SetName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new BussinessLogicException(SubjectErrors.NameCantBeNull);
+
+        Name = name;
+    }
+
+    public void SetDescription(string? description)
+    {
+        if (string.IsNullOrWhiteSpace(description))
+            throw new BussinessLogicException(SubjectErrors.DescriptionCantBeNull);
+
+        Description = description;
+    }
+
+    public class SubjectErrors
+    {
+        public static readonly Error NameCantBeNull = new(
+            "Subject.NameCantBeNull",
+            "Name cannot be null or empty."
+        );
+
+        public static readonly Error DescriptionCantBeNull = new(
+            "Subject.DescriptionCantBeNull",
+            "Description cannot be null or empty."
+        );
+    }
+}
