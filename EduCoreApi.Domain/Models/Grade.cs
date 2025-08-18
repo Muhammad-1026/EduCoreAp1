46dc2bab5b79950fb27f21426de6008aff843e13
MuhammadBekbutaev <bekbutaevmuhammad1026@gmail.com>
2025-08-18 10:31:49 +0500

    feat(domain): add Grade and Timetable models; update solution configuration

---
+++ EduCoreApi.Domain/Models/Grade.cs
@@ -0,0 +1,66 @@
+﻿using EduCoreApi.Domain.Common;
+using EduCoreApi.Shared.Exeptions;
+using EduCoreApi.Shared.Models;
+
+namespace EduCoreApi.Domain.Models;
+
+public class Grade : Entity
+{
+    public Guid StudentId { get; private set; }
+    public Student Student { get; private set; } = default!;
+
+    public Guid SubjectId { get; private set; }
+    public  Subject Subject { get; private set; } = default!;
+
+    public int Value { get; private set; }
+
+    public Grade(Guid studentId, Guid subjectId, int value)
+    {
+        StudentId = studentId;
+        SubjectId = subjectId;
+        Value = value;
+    }
+
+    public void SetStudentId(Guid studentId)
+    {
+        if (studentId == Guid.Empty)
+            throw new BussinessLogicException(GradeErrors.StudentIdCantBeNull);
+
+        StudentId = studentId;
+    }
+
+    public void SetSubjectId(Guid subjectId)
+    {
+        if (subjectId == Guid.Empty)
+            throw new BussinessLogicException(GradeErrors.SubjectIdCantBeNull);
+
+        SubjectId = subjectId;
+    }
+
+    public void SetValue(int value)
+    {
+        if (value < 0 || value > 100)
+            throw new BussinessLogicException(GradeErrors.ValueMustBeBetween0And100);
+
+        Value = value;
+    }
+
+    public class GradeErrors
+    {
+        public static readonly Error StudentIdCantBeNull = new(
+            "Grade.StudentIdCanBeNull",
+            "Student ID cannot be null."
+        );
+
+        public static readonly Error SubjectIdCantBeNull = new(
+            "Grade.SubjectIdCanBeNull",
+            "Subject ID cannot be null."
+        );
+
+        public static readonly Error ValueMustBeBetween0And100 = new(
+            "Grade.ValueMustBeBetween0And100",
+            "Grade value must be between 0 and 100."
+        );
+    }
+}
+
---
+++ EduCoreApi.Domain/Models/Timetable.cs
@@ -0,0 +1,106 @@
+﻿using EduCoreApi.Domain.Common;
+using EduCoreApi.Shared.Exeptions;
+using EduCoreApi.Shared.Models;
+
+namespace EduCoreApi.Domain.Models;
+
+public class Timetable : Entity
+{
+    public Guid SubjectId { get; private set; }
+    public Subject Subject { get; private set; } = default!;
+
+    public Guid GroupId { get; private set; }
+    public Group Group { get; private set; } = default!;
+
+    public DayOfWeek DayOfWeek { get; private set; } 
+
+    public TimeOnly StartTime { get; private set; }
+    public TimeOnly EndTime { get; private set; } 
+
+    public Timetable(Guid subjectId, Guid groupId, DayOfWeek dayOfWeek, TimeOnly startTime, TimeOnly endTime)
+    {
+        SubjectId = subjectId;
+        GroupId = groupId;
+        DayOfWeek = dayOfWeek;
+        StartTime = startTime;
+        EndTime = endTime;
+    }
+
+    public void SetSubjectId(Guid subjectId)
+    {
+        if (subjectId == Guid.Empty)
+            throw new BussinessLogicException(TimetableErrors.SubjectIdCantBeNull);
+
+        SubjectId = subjectId;
+    }
+
+    public void SetGroupId(Guid groupId)
+    {
+        if (groupId == Guid.Empty)
+            throw new BussinessLogicException(TimetableErrors.GroupIdCantBeNull);
+
+        GroupId = groupId;
+    }
+
+    public void SetDayOfWeek(DayOfWeek dayOfWeek)
+    {
+        if (dayOfWeek == default)
+            throw new BussinessLogicException(TimetableErrors.DayOfWeekCantBeNull);
+
+        DayOfWeek = dayOfWeek;
+    }
+
+    public void SetStartTime(TimeOnly startTime)
+    {
+        if (startTime < new TimeOnly(7, 0) || startTime > new TimeOnly(20, 0))
+            throw new BussinessLogicException(TimetableErrors.StartTimeInvalid);
+
+
+        StartTime = startTime;
+    }
+
+    public void SetEndTime(TimeOnly endTime)
+    {
+        if (endTime == default)
+            throw new BussinessLogicException(TimetableErrors.EndTimeCantBeNull);
+
+        if (endTime <= StartTime)
+            throw new BussinessLogicException(TimetableErrors.EndTimeMustBeGreaterThanStartTime);
+
+        EndTime = endTime;
+    }
+
+    public class TimetableErrors
+    {
+        public static readonly Error SubjectIdCantBeNull = new(
+            "Timetable.SubjectIdCantBeNull",
+            "Subject ID cannot be null."
+        );
+
+        public static readonly Error GroupIdCantBeNull = new(
+            "Timetable.GroupIdCantBeNull",
+            "Group ID cannot be null."
+        );
+
+        public static readonly Error DayOfWeekCantBeNull = new(
+            "Timetable.DayOfWeekCantBeNull",
+            "Day of the week cannot be null."
+        );
+
+        public static readonly Error StartTimeInvalid = new(
+            "Timetable.StartTimeInvalid",
+            "Start time is invalid."
+        );
+
+
+        public static readonly Error EndTimeCantBeNull = new(
+            "Timetable.EndTimeCantBeNull",
+            "End time cannot be null."
+        );
+
+        public static readonly Error EndTimeMustBeGreaterThanStartTime = new(
+            "Timetable.EndTimeMustBeGreaterThanStartTime",
+            "End time must be after start time."
+        );
+    }
+}
